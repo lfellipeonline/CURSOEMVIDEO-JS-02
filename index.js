2c7c031b0e14630a73814644441881140125079b
Luiz Felipe <mail.lfellipe@gmail.com>
2024-09-06 10:37:13 -0300

    commit aula 8

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-let nome = prompt('Olá! Bem vindo ao website da Abobora Inteligente. Qual o seu nome?')
+let nome = prompt(`Olá! Bem vindo ao website da Abobora Inteligente. Qual o seu nome?`)
     alert('Olá ' + nome + ' você entrou no website da abobora inteligente!') //Concatenação (+)
+    document.write(`Olá ${nome}, Seu nome tem ${nome.length} letras<br>`)
 
 let num1 = Number(prompt('Digite um numero:'))
 let num2 = Number(prompt('Digite outro numero:'))
 const soma = num1 + num2
-alert('O resultado da soma foi ' + soma)
+    document.write(`O resultado da soma foi ${String(soma)}`) //Conversão de tipo ${String()}
+
