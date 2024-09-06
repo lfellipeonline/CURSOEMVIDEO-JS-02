db96e70bde0565f93809918ed1cb563e325b70a4
Luiz Felipe <mail.lfellipe@gmail.com>
2024-09-05 21:09:48 -0300

    commit (estou indo pra casa tenho q dar commit)

---
+++ index.js
@@ -0,0 +1,7 @@
+let nome = prompt('Olá! Bem vindo ao website da Abobora Inteligente. Qual o seu nome?')
+    alert('Olá ' + nome + ' você entrou no website da abobora inteligente!') //Concatenação (+)
+
+let num1 = Number(prompt('Digite um numero:'))
+let num2 = Number(prompt('Digite outro numero:'))
+const soma = num1 + num2
+alert('O resultado da soma foi ' + soma)
