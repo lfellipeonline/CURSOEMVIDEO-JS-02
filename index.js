c1cea1e27f0b4d707c219ee298cf3e703c5bc2e0
Luiz Felipe <mail.lfellipe@gmail.com>
2024-09-06 13:04:12 -0300

    commit aula 8

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+//Atributos do nome
 let nome = prompt(`Olá! Bem vindo ao website da Abobora Inteligente. Qual o seu nome?`)
+let nomereplace = nome.toLowerCase().replace('abóbora', 'abobora') //Substituição de caracter
     alert('Olá ' + nome + ' você entrou no website da abobora inteligente!') //Concatenação (+)
     document.write(`Olá ${nome}, Seu nome tem ${nome.length} letras<br>`)
-
+    if (nomereplace == 'abobora')
+        document.write(`VOCÊ É UM DE NÓS!<br>`) //Condicional if
+    
+//Atributos dos números    
 let num1 = Number(prompt('Digite um numero:'))
 let num2 = Number(prompt('Digite outro numero:'))
 const soma = num1 + num2
-    document.write(`O resultado da soma foi ${String(soma)}`) //Conversão de tipo ${String()}
-
+    document.write(`O resultado da soma foi <b>${String(soma)}</b>`) //Conversão de tipo ${String()}
+    if (soma > 100000)
+        document.write('<br><b>Uau! Que grande número!</b>')
+   
+   
+   
+   
+   
+   
+console.log('Valor interno de nomereplace:', nomereplace)//Exibição no console
