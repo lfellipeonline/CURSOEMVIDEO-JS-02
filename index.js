17da37202982f27b2880ba2992f1d3fa57466022
Luiz Felipe <mail.lfellipe@gmail.com>
2024-09-10 13:13:04 -0300

    commit small update

--- index.js
+++ index.js
@@ -22,20 +22,15 @@ document.addEventListener('DOMContentLoaded', function() {
         window.alert('O resultado da soma é invalido!')
         window.location.reload()
     }
-
 })
+
 //Atributos do nome
 let nome = prompt(`Olá! Bem vindo ao website da Abobora Inteligente. Qual o seu nome?`)
 let nomereplace = nome.toLowerCase().replace('abóbora', 'abobora') //Substituição de caracter
 if (nome == '') {
     alert('Você não digitou um nome! Por favor, atualize a página.')
     window.location.reload()
-}
-alert('Olá ' + nome + ' você entrou no website da abobora inteligente!') //Concatenação (+)
-
-/*Deprecado
-document.write(`Olá ${nome}, Seu nome tem ${nome.length} letras<br>`)*/
-  
+}   alert('Olá ' + nome + ' você entrou no website da abobora inteligente!') //Concatenação (+)
 
 //Atributos dos números    
 let num1 = Number(prompt('Digite um numero:').replace(',', '.'))
@@ -44,21 +39,17 @@ if (num1 == '' && num2 == '') {
     alert('Você não digitou um número! Por favor, atualize a página.')
     window.location.reload()
 }
-
 const soma = num1 + num2
-
 if (Number.isNaN(soma)) {
     window.alert('Você não digitou um número! Por favor, atualize a página.')
     window.location.reload()
 }
 
-/*Deprecado
-    document.write(`O resultado da soma foi <b>${String(soma)}</b>`) //Conversão de tipo ${String()}*/
-
-//Atributos do website
+//Atributos do website (Os atributos abaixo são temporários até eu pensar no que fazer com esse botao)
 function clicar() {
-    alert('Você clicou no botão! (MAIS EM BREVE!)')
+    //alert('Você clicou no botão! (MAIS EM BREVE!)')
+    window.location.reload(true)
 }
-1
+
 //Exibição no console  
 console.log('Valor interno de nomereplace:', nomereplace)
